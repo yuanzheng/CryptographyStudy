b0f1c29d20dee93eae8e69e3193c1b75ac569b56
Yuanzheng Lucas Song <yuanzheng@cs.byu.edu>
2013-11-23 15:55:06 -0700

    init key size, add TODO plan

--- AES_BlockCipher_Java/AES_BlockCipher/src/AES.java
+++ AES_BlockCipher_Java/AES_BlockCipher/src/AES.java
@@ -1,4 +1,7 @@
-
+/**
+ * AES
+ * Block Cipher Modes: ECB, 
+ */
 import java.io.*;
 import java.math.BigInteger;
 import java.security.InvalidKeyException;
@@ -9,6 +12,8 @@ import javax.crypto.*;
 public class AES {
 	protected BufferedReader is = null;
 
+	private SecretKey aesKey = null;
+	
 	public String getContent(String fileName)
 	{
 		FileInputStream input = null;
@@ -30,57 +35,70 @@ public class AES {
 		return content.toString();
 	}
 	
-	public void Encryption(String fileName)
+	public void printFile(String fileName)
 	{
-		String content = getContent(fileName);
-		
-		KeyGenerator keygen = null;
 		try {
-			keygen = KeyGenerator.getInstance("AES");
+			BufferedReader input = new BufferedReader(new FileReader(fileName));
+			
+			String currLine;
+			while((currLine=input.readLine())!=null)
+			{
+				System.out.println(currLine);
+			}
+			input.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	// initialize AES key
+	public AES(int keySize)
+	{
+		try {
+			KeyGenerator keygen = KeyGenerator.getInstance("AES");
+			keygen.init(keySize);
+		    aesKey = keygen.generateKey();
 		} catch (NoSuchAlgorithmException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	    SecretKey aesKey = keygen.generateKey();
-		Cipher aesCipher = null;
-		try {
-			aesCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+		
+	}
+	
+	/**
+	 * TODO: 
+	 * CTR mode: http://www.java2s.com/Code/JavaAPI/javax.crypto/CipherENCRYPTMODE.htm
+	 * 
+	 * AES/CBC/NoPadding (128)
+	 * AES/CBC/PKCS5Padding (128)
+	 * AES/ECB/NoPadding (128)
+	 * AES/ECB/PKCS5Padding (128)
+	 */	
+	public void Encryption(String fileName, String mode)
+	{
+		String content = getContent(fileName);
+		
+	    try {
+	    	Cipher aesCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+			// Initialize the cipher for encryption
+			aesCipher.init(Cipher.ENCRYPT_MODE, aesKey);
+			byte[] ciphertext = aesCipher.doFinal(content.getBytes());
+			
+			printCipherText(ciphertext);
+			
 		} catch (NoSuchAlgorithmException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (NoSuchPaddingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		// Initialize the cipher for encryption
-	    try {
-			aesCipher.init(Cipher.ENCRYPT_MODE, aesKey);
 		} catch (InvalidKeyException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-
-	    try {
-			byte[] ciphertext = aesCipher.doFinal(content.getBytes());
-			for(byte b : ciphertext)
-			{
-				System.out.print(String.format("%x", b));
-			}
-			System.out.println();
-			
-			// The difference between solution above and down !!!!!
-			for(byte b : ciphertext)
-			{
-				System.out.print(String.format("%02x", b));
-			}
-			System.out.println();
-			
-			// Better way ...
-			BigInteger bi = new BigInteger(1, ciphertext);
-			String result = String.format("%0" + (ciphertext.length<<1) + "X", bi);
-			System.out.print(result);
-			
-			
 		} catch (IllegalBlockSizeException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -88,17 +106,48 @@ public class AES {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	    
+	}
+	
+	
+	// TODO
+	public void Decryption()
+	{
+		
+	}
+	
+	private void printCipherText(byte[] ciphertext)
+	{
+		for(byte b : ciphertext)
+		{
+			System.out.print(String.format("%x", b));
+		}
+		System.out.println();
+		
+		// The difference between solution above and down !!!!!
+		for(byte b : ciphertext)
+		{
+			System.out.print(String.format("%02x", b));
+		}
+		System.out.println();
+		
+		// Better way ...
+		BigInteger bi = new BigInteger(1, ciphertext);
+		String result = String.format("%0" + (ciphertext.length<<1) + "X", bi);
+		System.out.print(result);
 		
 	}
         
 	public static void main(String[] args)
 	{
-		AES encryption = new AES();
+		int size = 128;
+		AES encryption = new AES(size);
+		// TEST read file correctly
 		String content = encryption.getContent("plaintext.txt");
 		System.out.println(content.toString());
+		encryption.printFile("plaintext.txt");
 		
-		encryption.Encryption("plaintext.txt");
+		// Encrypt Message
+		encryption.Encryption("plaintext.txt", "ECB");
 		
 	}
 
